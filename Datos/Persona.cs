3dc7385c8f4fdeacb8fd1cfc9538ddcb21fd50d6
Matias Bais <matiasbais1998@gmail.com>
2023-08-05 01:08:11 -0300

    a

---
+++ Datos/Persona.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Configuration;
+
+namespace Datos
+{
+    public class Persona
+    {
+        string connectionString = ConfigurationManager.ConnectionStrings["YourConnectionStringKey"].ConnectionString;
+
+
+
+    }
+}
---
+++ Entidades/Persona.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Persona : Padre
+    {
+
+        public string Nombre { get; set; }
+
+        public string Apellido { get; set; }    
+
+        public string Email { get; set; }
+
+        public string Telefono { get; set; }
+
+        public string Direccion { get; set; }
+
+        public DateTime FechaNacimiento  { get; set; }  
+
+        public int Legajo { get; set; }
+
+        public string TipoPersona { get; set; }
+
+        public Plan Plan { get; set; }
+    }
+}
