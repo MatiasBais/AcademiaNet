c190bfed16caef0d4d0653ae2fb1656b6adf9ff6
nazanecchi <62289237+nazanecchi@users.noreply.github.com>
2023-09-28 11:28:44 -0300

    cambiado el sql conection a bdd en azure
    pornhub redirection

--- Datos/Base.cs
+++ Datos/Base.cs
@@ -9,8 +9,6 @@ namespace Datos
 {
     public class Base
     {
-
-        public SqlConnection conn = new SqlConnection(@"Server=MATIASESCRITORI\TEW_SQLEXPRESS;Database=academia;Trusted_Connection=True;");
-
+        public SqlConnection conn = new SqlConnection(@"Server=tcp:academianet.database.windows.net,1433;Initial Catalog = academia2; Persist Security Info=False;User ID = netadmin; Password=Qwer1234; MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout = 30;");
     }
 }
